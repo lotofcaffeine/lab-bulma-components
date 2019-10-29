52777f12c223aefe54feeda2cb001f46cfd2cc5b
Augusta Raposo <gutaraposomarques@gmail.com>
2019-10-29 19:31:16 +0100

    update

--- start-code/src/components/App.js
+++ start-code/src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import FormField from "./FormField";
 
 class App extends Component {
   render() {
     return (
       <div>
         <NavBar></NavBar>
+        <div className="form" style={{ padding: "7% 7%", marginTop: "100px" }}>
+          <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+          <FormField
+            label="Email"
+            type="email"
+            placeholder="e.g. alexsmith@gmail.com"
+          />
+        </div>
       </div>
     );
   }
--- start-code/src/components/FormField.js
+++ start-code/src/components/FormField.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 
 class FormField extends Component {
   render() {
-    return (<div className="field">
-        <label htmlFor=""></label>
-        <div></div>
-    </div>;
+    return (
+      <div className="field">
+        <label>{this.props.label}</label>
+        <div className="control">
+          <input
+            className="input"
+            type={this.props.type}
+            placeholder={this.props.placeholder}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
 export default FormField;
-    )
